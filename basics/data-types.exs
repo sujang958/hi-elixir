00c96b42eda5d2cea5713ceece0c9cab157344fc
sujang958 <enter7377@naver.com>
2022-07-05 23:50:36 +0900

    feat: add data types

---
+++ basics/data-types.exs
@@ -0,0 +1,30 @@
+IO.puts "This is an interger, it can be bin, hex or oct too"
+IO.puts 244
+
+IO.puts ""
+
+IO.puts "This is a floating point. At least, it requires a number back of the decimal point"
+IO.puts 3.141592653589793
+
+IO.puts ""
+
+IO.puts "This is an atom. Atom is a constant whose name is its value."
+IO.puts :ATOMMM
+IO.puts "And names of modules are also atoms."
+
+IO.puts ""
+
+IO.puts "This is a boolean, booleans are atoms."
+IO.puts "So true equals :true"
+IO.puts true === :true
+
+IO.puts ""
+
+IO.puts "This is a string, which is UTF-8 encoded"
+IO.puts "Strings are wrapped in double quotes"
+IO.puts "Strings support line
+breaks"
+
+IO.puts ""
+
+
